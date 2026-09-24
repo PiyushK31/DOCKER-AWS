f2b45717662d318821daec4129407549ecfc3266
PiyushK31 <piyushkrr80580@gmail.com>
2026-09-24 22:12:54 +0530

    Add favicon, icons, and index.html for frontend setup
    - Created favicon.svg for the application icon.
    - Added icons.svg containing various SVG icons for use in the application.
    - Introduced index.html as the main entry point for the frontend, linking to the favicon and necessary assets.

--- Backend/server.js
+++ Backend/server.js
@@ -1,36 +1,37 @@
-import { error } from "console";
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { YSocketIO } from "y-socket.io/dist/server";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
 const httpServer = createServer(app);
 
-const io =new Server(httpServer, {
+const io = new Server(httpServer, {
     cors: {
         origin: "*",
-        methods: ["GET" , "POST"]
+        methods: ["GET", "POST"]
     }
-})
+});
 
-const ySocketIO = new YSocketIO(io)
-ySocketIO.initialize()
+const ySocketIO = new YSocketIO(io);
 
-
-app.get("/", (req, res) =>{
-    res.status(200).json({
-        message: "Server is running",
-        success: true
-    })
-})
+ySocketIO.initialize();
 
 app.get('/health', (req, res) => {
     res.status(200).json({
         message: "Server is Healthy",
         success: true
-    })
-})
+    });
+});
 
 httpServer.listen(3000, (err) => {
     if (err) {
@@ -38,4 +39,4 @@ httpServer.listen(3000, (err) => {
     } else {
         console.log("Server is running on port 3000");
     }
-})
+});
